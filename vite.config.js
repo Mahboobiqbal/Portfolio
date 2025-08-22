1329539a07c4a51f4fc33b3797b3dbaefd7a40f4
Mahboob Iqbal <mahboobiqbal774@gmail.com>
2025-08-22 23:34:22 +0500

    config updated

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/",
   server: {
     port: 3000,
-    host: '127.0.0.1', // Use IPv4 loopback
+    host: "127.0.0.1", // Use IPv4 loopback
   },
 });
